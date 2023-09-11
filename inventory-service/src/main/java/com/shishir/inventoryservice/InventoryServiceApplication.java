650d0b0be32af86b5e2646747b9871d0833b8aa0
shishirkarki <skarki2@gmail.com>
2023-09-11 09:42:51 +0545

    feat: Eureka Client Setup

--- inventory-service/src/main/java/com/shishir/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/shishir/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,13 @@ import com.shishir.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
